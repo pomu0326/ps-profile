569255279b185566d6a8a8c839d10d358807bf75
k.nomura <k.nomura@appguard-m.com>
2023-10-05 17:05:01 +0900

    chore: インストールスクリプトを追加する

---
+++ ps-profile.build.ps1
@@ -0,0 +1,11 @@
+﻿task Install {
+    $Profile = "$PSScriptRoot\Microsoft.PowerShell_profile.ps1"
+    $Target = @(
+        "${env:UserProfile}\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+        "${env:UserProfile}\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1"
+        "${env:UserProfile}\Documents\PowerShell\Microsoft.PowerShell_profile.ps1"
+        "${env:UserProfile}\Documents\PowerShell\Microsoft.VSCode_profile.ps1"
+    )
+    $Target `
+    | ForEach-Object { New-Item -Type HardLink -Path $Profile -Target $_ -Force }
+}
